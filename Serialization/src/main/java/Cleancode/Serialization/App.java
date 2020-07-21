9764812b2b864b1101f5f68aad960e76718e136e
91957 <91957@192.168.2.2>
2020-07-21 20:33:05 +0530

    input output serialization

---
+++ Serialization/src/main/java/Cleancode/Serialization/App.java
@@ -0,0 +1,13 @@
+package Cleancode.Serialization;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
